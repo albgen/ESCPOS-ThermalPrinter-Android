22b50045fd72a5c9eeb86ef93129c09ba50b8e52
evald80 <evald80@gmail.com>
2024-11-27 16:40:04 +0100

    feat: add sendDelay parameter for transmission timing in addition to addWaitingTime
    - Add sendDelay parameter to control transmission timing for data.
    Benefits:
        Flexibility: You can dynamically adjust the delay based on device requirements.
        Backward Compatibility: Existing methods work without modifications if sendDelay is not explicitly set.
        Improved Control: Delays can help prevent buffer overflows or ensure proper synchronization with slower devices.

--- escposprinter/src/main/java/com/dantsu/escposprinter/connection/DeviceConnection.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/connection/DeviceConnection.java
@@ -8,13 +8,16 @@ import java.io.OutputStream;
 public abstract class DeviceConnection {
     protected OutputStream outputStream;
     protected byte[] data;
+    protected int sendDelay; // Added sendDelay parameter
 
     public DeviceConnection() {
         this.outputStream = null;
         this.data = new byte[0];
+        this.sendDelay = 0; // Default send delay set to 0
     }
 
     public abstract DeviceConnection connect() throws EscPosConnectionException;
+
     public abstract DeviceConnection disconnect();
 
     /**
@@ -27,6 +30,24 @@ public abstract class DeviceConnection {
     }
 
     /**
+     * Set the send delay in milliseconds.
+     *
+     * @param delay Delay time in milliseconds
+     */
+    public void setSendDelay(int delay) {
+        this.sendDelay = delay;
+    }
+
+    /**
+     * Get the current send delay in milliseconds.
+     *
+     * @return Current send delay
+     */
+    public int getSendDelay() {
+        return this.sendDelay;
+    }
+
+    /**
      * Add data to send.
      */
     public void write(byte[] bytes) {
@@ -36,26 +57,26 @@ public abstract class DeviceConnection {
         this.data = data;
     }
 
-
     /**
      * Send data to the device.
      */
     public void send() throws EscPosConnectionException {
         this.send(0);
     }
+
     /**
-     * Send data to the device.
+     * Send data to the device with additional waiting time.
      */
     public void send(int addWaitingTime) throws EscPosConnectionException {
-        if(!this.isConnected()) {
+        if (!this.isConnected()) {
             throw new EscPosConnectionException("Unable to send data to device.");
         }
         try {
             this.outputStream.write(this.data);
             this.outputStream.flush();
-            int waitingTime = addWaitingTime + this.data.length / 16;
+            int waitingTime = addWaitingTime + this.data.length / 16 + this.sendDelay; // Include sendDelay
             this.data = new byte[0];
-            if(waitingTime > 0) {
+            if (waitingTime > 0) {
                 Thread.sleep(waitingTime);
             }
         } catch (IOException | InterruptedException e) {
